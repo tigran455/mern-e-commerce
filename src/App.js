5da2b6daa3f93d7b03e37ba2717030dc8f76f2eb
Tigran <tigran19910315@gmail.com>
2024-08-02 17:33:49 +0400

    react-router-dom instalation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import {Outlet} from "react-router-dom";
 
 function App() {
   return (
-    <div className="text-red-600">
-      MERN APP
-    </div>
+    <>
+        <Outlet/>
+    </>
   );
 }
 
