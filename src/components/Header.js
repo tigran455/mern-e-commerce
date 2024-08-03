47c475d4d9f0b6c59a190d7394be640af78c00ec
Tigran <tigran19910315@gmail.com>
2024-08-03 13:45:38 +0400

    login form

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,14 +3,17 @@ import Logo from "./Logo";
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa";
+import {Link} from "react-router-dom";
 
 
 const Header = () => {
     return (
-        <header className='h-16 shadow-md'>
+        <header className='h-16 shadow-md bg-white'>
             <div className='h-full container mx-auto flex items-center px-4 justify-between'>
                 <div>
-                    <Logo w={90} h={50}/>
+                    <Link to='/'>
+                        <Logo w={90} h={50}/>
+                    </Link>
                 </div>
                 <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow pl-2'>
                     <input type='text' placeholder='search product here...' className='w-full outline-none'/>
@@ -31,9 +34,9 @@ const Header = () => {
                         </div>
                     </div>
                     <div>
-                        <button className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>
+                        <Link to='login' className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>
                             Login
-                        </button>
+                        </Link>
 
                     </div>
                 </div>
---
+++ src/pages/Login.js
@@ -0,0 +1,48 @@
+import React, {useState} from 'react';
+import loginIcon from '../assest/signin.gif'
+import {FaEye, FaEyeSlash} from "react-icons/fa";
+import {Link} from "react-router-dom";
+
+const Login = () => {
+    const [showPassword, setShowPassword] = useState(false)
+    return (
+        <section id='login'>
+            <div className='mx-auto container p-4'>
+                <div className='bg-white p-2 py-5 w-full max-w-md mx-auto'>
+                    <div className='w-20 h-20 mx-auto'>
+                        <img src={loginIcon} alt='login icon'/>
+                    </div>
+                    <form>
+                        <div className='grid'>
+                            <label>Email :</label>
+                            <div className='bg-slate-100 p-2'>
+                                <input type='email' placeholder='enter email' className='w-full h-full outline-none bg-transparent'/>
+                            </div>
+                        </div>
+                        <div>
+                            <label>Password :</label>
+                            <div className='bg-slate-100 p-2 flex'>
+                                <input type={showPassword ? 'text' : 'password'} placeholder='enter password' className='w-full h-full outline-none bg-transparent'/>
+                                <div className='cursor-pointer text-xl' onClick={()=>setShowPassword((prev)=> !prev)}>
+                                    <span>
+                                        {
+                                            showPassword
+                                            ? <FaEye/>
+                                            : <FaEyeSlash/>
+                                        }
+                                    </span>
+                                </div>
+                            </div>
+                            <Link to='/forgot-password' className='block w-fit ml-auto hover:underline hover:text-red-600'>
+                                Forgot password
+                            </Link>
+                        </div>
+                        <button className='bg-red-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Login;
