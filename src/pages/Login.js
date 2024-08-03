be71c8431223493a03edc2725e7c3373ed0ea74f
Tigran <tigran19910315@gmail.com>
2024-08-03 14:19:30 +0400

    input onchange

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -5,24 +5,55 @@ import {Link} from "react-router-dom";
 
 const Login = () => {
     const [showPassword, setShowPassword] = useState(false)
+    const [data, setData] = useState({
+        email: "",
+        password: ""
+    })
+    const handleOnChange = (e) => {
+        const {name, value} = e.target
+        setData((prev)=>{
+            return{
+                ...prev,
+                [name]: value
+            }
+        })
+
+    }
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+
+    }
+    console.log(data)
     return (
         <section id='login'>
-            <div className='mx-auto container p-4'>
-                <div className='bg-white p-2 py-5 w-full max-w-md mx-auto'>
+            <div className='mx-auto container p-5'>
+                <div className='bg-white p-4 w-full max-w-sm mx-auto'>
                     <div className='w-20 h-20 mx-auto'>
                         <img src={loginIcon} alt='login icon'/>
                     </div>
-                    <form>
+                    <form className='pt-6' onSubmit={handleSubmit}>
                         <div className='grid'>
                             <label>Email :</label>
                             <div className='bg-slate-100 p-2'>
-                                <input type='email' placeholder='enter email' className='w-full h-full outline-none bg-transparent'/>
+                                <input
+                                    type='email'
+                                    placeholder='enter email'
+                                    name='email'
+                                    value={data.email}
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'/>
                             </div>
                         </div>
                         <div>
                             <label>Password :</label>
                             <div className='bg-slate-100 p-2 flex'>
-                                <input type={showPassword ? 'text' : 'password'} placeholder='enter password' className='w-full h-full outline-none bg-transparent'/>
+                                <input
+                                    type={showPassword ? 'text' : 'password'}
+                                    placeholder='enter password'
+                                    name='password'
+                                    value={data.password}
+                                    onChange={handleOnChange}
+                                    className='w-full h-full outline-none bg-transparent'/>
                                 <div className='cursor-pointer text-xl' onClick={()=>setShowPassword((prev)=> !prev)}>
                                     <span>
                                         {
@@ -34,11 +65,15 @@ const Login = () => {
                                 </div>
                             </div>
                             <Link to='/forgot-password' className='block w-fit ml-auto hover:underline hover:text-red-600'>
-                                Forgot password
+                                Forgot password ?
                             </Link>
                         </div>
-                        <button className='bg-red-600 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
+                        <button className='bg-red-600 hover:bg-red-700 text-white px-6 py-2 w-full max-w-[150px] rounded-full hover:scale-110 transition-all mx-auto block mt-6'>Login</button>
                     </form>
+                    <p className='my-5'>
+                        Don't have account ?
+                        <Link to='/sign-up' className='text-red-600 hover:text-red-700 hover:underline'>Sign up</Link>
+                    </p>
                 </div>
             </div>
         </section>
